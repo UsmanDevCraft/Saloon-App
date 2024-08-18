fd7c97eac3c4aee2283001eb82a0b12854dab940
Usman Awan <uawan8154@gmail.com>
2024-08-18 13:19:00 +0100

    added changes to perform build in vercel.

--- pages/staff.js
+++ pages/staff.js
@@ -1,6 +1,29 @@
-import { Staff as importedstaff} from "@/sections"
-import Head from "next/head"
-import React from "react"
+// import { Staff as importedstaff} from "@/sections"
+// import Head from "next/head"
+// import React from "react"
+
+// const Staff = () => {
+//   return (
+//     <>
+//       <Head>
+//         <title>Staff - Curl Up and Dye</title>
+//       </Head>
+//       <Staff />
+//     </>
+//   )
+// }
+
+// export default Staff
+
+
+
+
+
+//GPT CODE TO RUN BUILD
+// pages/staff.js
+import { Staff as StaffSection } from "@/sections"; // Renamed import
+import Head from "next/head";
+import React from "react";
 
 const Staff = () => {
   return (
@@ -8,9 +31,9 @@ const Staff = () => {
       <Head>
         <title>Staff - Curl Up and Dye</title>
       </Head>
-      <Staff />
+      <StaffSection /> {/* Render the imported component */}
     </>
-  )
-}
+  );
+};
 
-export default Staff
+export default Staff;
--- sections/Staff.js
+++ sections/Staff.js
@@ -1,8 +1,39 @@
-import { staffdata } from "@/assets/data/dummydata"
-import { Card } from "@/components/common/Card"
-import { Title, TitleSm } from "@/components/common/Title"
-import team from "@/pages/staff"
-import React from "react"
+// import { staffdata } from "@/assets/data/dummydata"
+// import { Card } from "@/components/common/Card"
+// import { Title, TitleSm } from "@/components/common/Title"
+// import team from "@/pages/staff"
+// import React from "react"
+
+// const Staff = () => {
+//   return (
+//     <>
+//       <section className='agency bg-top'>
+//         <div className='container'>
+//           <div className='heading-title'>
+//             <TitleSm title='MEET OUR STAFF' /> <br />
+//             <br />
+//             <Title title='A staff of smart & passionate creatives' className='title-bg' />
+//           </div>
+//           <div className='grid-4 py'>
+//             {teamdata.map((item) => (
+//               <Card data={item} key={item.id} caption={item.post} />
+//             ))}
+//           </div>
+//         </div>
+//       </section>
+//     </>
+//   )
+// }
+
+// export default Staff
+
+
+
+//GPT CODE TO RUN BUILD
+import { staffdata } from "@/assets/data/dummydata"; // Ensure this is the correct data import
+import { Card } from "@/components/common/Card";
+import { Title, TitleSm } from "@/components/common/Title";
+import React from "react";
 
 const Staff = () => {
   return (
@@ -15,14 +46,14 @@ const Staff = () => {
             <Title title='A staff of smart & passionate creatives' className='title-bg' />
           </div>
           <div className='grid-4 py'>
-            {teamdata.map((item) => (
+            {staffdata.map((item) => (
               <Card data={item} key={item.id} caption={item.post} />
             ))}
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Staff
+export default Staff;
